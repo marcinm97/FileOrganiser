af0bc6582e2d8cb9d47435d2cd139ec5fdf25a4f
Marcin Mucha <marcin485@op.pl>
2019-05-23 16:28:18 +0200

    Local init commit

---
+++ cmake-build-debug/CMakeFiles/FileOrganiser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileOrganiser.dir/main.cpp.obj"
+  "FileOrganiser.pdb"
+  "FileOrganiser.exe"
+  "FileOrganiser.exe.manifest"
+  "libFileOrganiser.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileOrganiser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
