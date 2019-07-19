422253372d3ead95b9095bc793932a942d095c3b
Marcin Mucha <marcin485@op.pl>
2019-07-19 20:34:35 +0200

    Add short message for users

--- main.cpp
+++ main.cpp
@@ -16,6 +16,15 @@ int main() {
 
     manager.run();
 
+    // 1. If You want to change file name according to your own .txt file, you should add
+    // "filenamesList.txt" to cmake-build-debug directory. File should content list of names and surnames
+    // such like this:
+
+    // 1. Andrey Martin    < -
+    // 2. Kate Brown       < -
+    // 3. John Drows       < -
+    // ...
+
     //manager.showContentedFiles();
 
     return 0;
